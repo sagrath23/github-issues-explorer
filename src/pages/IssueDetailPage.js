ace0ed37aa6b3acc996675f21531d5dac1288d1f
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-08-02 22:25:40 -0500

    add hooks to use URL params & select issue from state

--- src/pages/IssueDetailPage.js
+++ src/pages/IssueDetailPage.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { selectedIssueSelector } from '../selectors';
 
-export const IssueDetailPage = () => (
-  <div>Issue detail</div>
-);
+export const IssueDetailPage = () => {
+  const dispatch = useDispatch();
+  const { issueId } = useParams();
+  const selectedIssue = useSelector(selectedIssueSelector);
+
+  console.log(issueId, 'da params');
+  console.log(selectedIssue);
+
+  return (
+    <div>
+      <h1>Issue #{issueId}</h1>
+    </div>
+  );
+};
--- src/selectors/selectors.js
+++ src/selectors/selectors.js
@@ -1,3 +1,5 @@
 export const searchTermSelector = (state) => state.search.searchTerm;
 
 export const searchResultsSelector = (state) => state.search.results;
+
+export const selectedIssueSelector = (state) => state.search.selectedIssue;
