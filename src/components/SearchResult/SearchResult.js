96f939e471a37d705ade82cade0ce234b8cdc5d0
Steven Villegas <sagrath23@gmail.com>
2020-08-06 09:19:13 -0500

    add more unit tests

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { v4 as uuidv4 } from 'uuid';
 
-
-export const AutoCompleteResult = ({ number, title }) => (
-  <Link key={uuidv4()} className="list-group-item list-group-item-action" to={`/issues/${number}`}>
-    <p class="lead m-0">
+export const SearchResult = ({ number, title }) => (
+  <Link className="list-group-item list-group-item-action" to={`/issues/${number}`}>
+    <p className="lead m-0">
       {title}
     </p>
   </Link>
 );
 
-AutoCompleteResult.propTypes = {
+SearchResult.propTypes = {
   number: PropTypes.number.isRequired,
   title: PropTypes.string
 };
 
-AutoCompleteResult.defaultProps = {
+SearchResult.defaultProps = {
   title: ''
 };
--- src/components/SearchResult/SearchResult.spec.js
+++ src/components/SearchResult/SearchResult.spec.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 import { SearchResult } from '../SearchResult';
+import { renderRoutingComponent } from '../../utils';
 
 describe('SearchResult component', () => {
-  test('Should render component with default props', () => {
-    expect(true).toBeTruthy();
+  const resultTitle = 'result 0';
+
+  test('Should render component', () => {
+    renderRoutingComponent(SearchResult, { number: 0, title: resultTitle });
+    expect(screen.getByText(resultTitle)).toBeDefined();
   });
 });
