5726a1e1d3a8834dc635c048e1123746d9eac3f2
Steven Villegas <sagrath23@gmail.com>
2020-08-06 07:28:22 -0500

    rename component

---
+++ src/components/SearchResult/SearchResult.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SearchResult } from '../SearchResult';
+
+describe('SearchResult component', () => {
+  test('Should render component with default props', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- src/components/SearchResultList/SearchResultList.js
+++ src/components/SearchResultList/SearchResultList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
-import { AutoCompleteResult } from '../AutoCompleteResult';
+import { SearchResult } from '../SearchResult';
 
 export const SearchResultList = ({ results }) => (
   <div className="list-group">
     {results.length > 0 && (
       <>
         {results.map((result) => (
-          <AutoCompleteResult key={uuidv4()} number={result.number} title={result.title} />
+          <SearchResult key={uuidv4()} number={result.number} title={result.title} />
         ))}
       </>
     )}
